fadc14f7a7e2b26d46144ccc2cf8600954430b76
MatildaWintence <matilda@wintence.com>
2022-10-06 23:21:32 +0200

    Changed my tests to something I think is more what I was looking for. Still not sure..

--- src/test/java/com/edufy/mostplayed/edufymostplayed/service/UserMediaListeningsServiceTest.java
+++ src/test/java/com/edufy/mostplayed/edufymostplayed/service/UserMediaListeningsServiceTest.java
@@ -1,15 +1,11 @@
 package com.edufy.mostplayed.edufymostplayed.service;
 
 import com.edufy.mostplayed.edufymostplayed.model.Media;
-import com.edufy.mostplayed.edufymostplayed.model.User;
-import com.edufy.mostplayed.edufymostplayed.model.UserMediaListening;
-import com.edufy.mostplayed.edufymostplayed.repository.MediaRepo;
-import com.edufy.mostplayed.edufymostplayed.repository.UserMediaListeningRepo;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
 
 import java.time.LocalDate;
 
@@ -17,12 +13,20 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class UserMediaListeningsServiceTest {
 
-    @MockBean
-    private UserMediaListeningRepo userMediaListeningRepo;
-    @MockBean
-    private MediaRepo mediaRepo;
+    @Mock
+    private UserMediaListeningsService userMediaListeningsService;
 
     @Test
+    void shouldGetTopMedia() {
+        Media testMedia = new Media(2, "url", LocalDate.of(2022, 10, 22), "Woho");
+        Mockito.when(userMediaListeningsService.getTopMedia(5))
+                .thenReturn(testMedia);
+        assertEquals(testMedia, userMediaListeningsService.getTopMedia(5));
+    }
+
+    // Tester som jag är osäker på..
+
+    /*@Test
     void shouldGetTopMedia() {
         //User testUser = new User(3, "MatWin", "User", "Lösenord");
         Media testMedia = new Media(5, "url", LocalDate.of(1990, 12, 06), "Yaaa");
@@ -30,4 +34,14 @@ class UserMediaListeningsServiceTest {
                         .thenReturn(testMedia);
         assertEquals(testMedia, mediaRepo.findMediaById(userMediaListeningRepo.findMediaIdOnUserNative(3)));
     }
+
+    @Test
+    void shouldGetTopMediaOnUserId() {
+        //User testUser = new User(3, "MatWin", "User", "Lösenord");
+        Media testMedia = new Media(2, "url", LocalDate.of(2022, 10, 22), "Woho");
+        //UserMediaListening userMediaListening = new UserMediaListening(1, testUser, testMedia);
+        Mockito.when(userMediaListeningsService.getTopMedia(3))
+                .thenReturn(testMedia);
+        assertEquals("Woh", userMediaListeningsService.getTopMedia(3).getMedianame());
+    }*/
     }
